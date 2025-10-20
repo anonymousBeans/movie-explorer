43cadca39761b2d238db467036778c3a8289ac14
Daniel Limbach <limbac02@ads.uni-passau.de>
2025-10-20 15:24:05 +0200

    feat(ui-grid): set up grid for the movie cards and changed the theme to dark

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import MovieCard from "../components/MovieCard";
+
+const movies = [
+  { title: "Movie One", year: 2024, genre: "Action" },
+  { title: "Movie Two", year: 2023, genre: "Drama" },
+  { title: "Movie Three", year: 2022, genre: "Sci-Fi" },
+];
 
 function Home() {
   return (
-    <h2>Home – Suche & Grid</h2>
-  )
+    <div className="container py-4">
+      <div className="row g-3 justify-content-center">
+        {movies.map((movie, index) => (
+          <div key={index} className="col-6 col-md-4 col-lg-3">
+            <MovieCard movie={movie} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
